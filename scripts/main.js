48927c0adf6f5747efe6dbd6b273b72cf9c8137c
erichanliu <ericliu404@gmail.com>
2019-03-28 16:56:21 -0400

    Add user absorption of enemies
    Haven't implemented losing based on radius size, or correct radius increase. Will need to create neutral objects that increase radius more reasonably, and enemies that can move

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ let ctx = canvas.getContext('2d');
 
 class Ball
 {
-    constructor(x, y, r, speed, color)
+    constructor(x, y, r, speed, color, id)
     {
         this.x = x;
         this.y = y;
@@ -14,6 +14,7 @@ class Ball
         this.uP = null;
         this.lP = null;
         this.rP = null;
+        this.id = id;
     }
 }
 
@@ -22,23 +23,30 @@ function main()
     // create player ball
     let midX = window.innerWidth / 2;
     let midY = window.innerHeight / 2;
-    let player = new Ball(midX, midY, 10, 7, 'tomato');
-
-    // create enemy ball
-    let x = getRandomInt(0, canvas.width);
-    let y = getRandomInt(0, canvas.height);
-    let r = getRandomInt(player.r / 2, player.r * 2);
-    let speed = getRandomInt(player.speed / 2, player.speed * 2);
-    let enemy = new Ball(x, y, r, speed, 'blue');
+    let player = new Ball(midX, midY, 10, 7, 'tomato', 0);
 
     setCanvasSize();
     attachListeners(player);
 
-    // game loop
+    // generate enemies
+    let enemies = [];
+    let enemyGenInterval = 2000;
+    let enemyId = 1;
+    setInterval(() => {
+        // create enemy ball
+        let attr = getRandomAttributes(player);
+        enemies.push(new Ball(attr.x, attr.y, attr.r, attr.speed, 'blue', enemyId));
+        enemyId++;
+    }, enemyGenInterval);
+
+    // draw loop
     function loop ()
     {
+        // clear canvas
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
         drawPlayer(player);
-        drawEnemy(enemy);
+        drawEnemies(enemies, player);
         movePlayer(player);
         window.requestAnimationFrame(loop);
     }
@@ -47,17 +55,45 @@ function main()
 
 main();
 
+// returns random attributes scaled to a player
+function getRandomAttributes(player)
+{
+    let x = getRandomInt(0, window.innerWidth);
+    let y = getRandomInt(0, window.innerHeight);
+    let r = getRandomInt(player.r / 2, player.r * 2);
+    let speed = getRandomInt(player.speed / 2, player.speed * 2);
+    return {
+        x: x,
+        y: y,
+        r: r,
+        speed: speed
+    }
+}
+
 function drawPlayer(player)
 {
-    // clear canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
     draw(player);
 }
 
 // create an enemy ball
-function drawEnemy(enemy)
+function drawEnemies(enemies, player)
 {
-    draw(enemy);
+    if (enemies === []) {
+        return;
+    }
+    for (let i = 0; i < enemies.length; i++)
+    {
+        let e = enemies[i];
+        if (circleCollision(player, e)) {
+            // TODO: make deletion depend on radii
+            // remove enemy from enemies array, increase radius size
+            let index = enemies.indexOf(e);
+            enemies.splice(index, 1);
+            player.r += Math.floor(e.r / 2);
+        } else {
+            draw(e);
+        }
+    }
 }
 
 function movePlayer(player)
@@ -128,4 +164,9 @@ function getRandomInt(min, max)
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function circleCollision(c1, c2)
+{
+    return (Math.pow(c2.x - c1.x, 2) + Math.pow(c1.y - c2.y, 2) <= Math.pow(c1.r + c2.r, 2));
 }
